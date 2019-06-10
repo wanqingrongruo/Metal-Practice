c059f414a3b45846005ab048bb088a33f73ca041
devzheng <1210538084@qq.com>
2019-06-10 17:26:37 +0800

    code

--- chapter_03/iOS+Swift/start/MetalDemo/MetalDemo/Metal/View/YLZMetalView.swift
+++ chapter_03/iOS+Swift/start/MetalDemo/MetalDemo/Metal/View/YLZMetalView.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class YLZMetalView: UIView {
 
+    var device: MTLDevice?
+
+    var metalLayer: CAMetalLayer {
+        return layer as! CAMetalLayer
+    }
+
+    var commonQueue: MTLCommandQueue?
+
+    override class var layerClass : AnyClass {
+        return CAMetalLayer.self
+    }
+
     // MARK: - Life Cycle
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,6 +35,33 @@ class YLZMetalView: UIView {
     
     // MARK: - Private
     func commonInit() {
-        
+
+        device = MTLCreateSystemDefaultDevice()
+        commonQueue = device?.makeCommandQueue()
+    }
+
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+
+        render()
+    }
+
+    func render() {
+        guard let drawable = metalLayer.nextDrawable() else {
+            return
+        }
+
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        let colorAttachment = renderPassDescriptor.colorAttachments[0]
+        colorAttachment?.clearColor = MTLClearColorMake(0.48, 0.74, 0.92, 1)
+        colorAttachment?.texture = drawable.texture  // 渲染目标
+        colorAttachment?.loadAction = .clear // 决定前一次 texture 的内容需要清除、还是保留
+        colorAttachment?.storeAction = .store // 当 loadAction 是 MTLLoadActionClear 时，则会使用对应的颜色来覆盖当前 texture（用某一色值逐像素写入)
+
+        let commandBuffer = commonQueue?.makeCommandBuffer()
+        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        commandEncoder?.endEncoding()
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
     }
 }
